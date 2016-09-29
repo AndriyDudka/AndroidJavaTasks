c975700f08291525ffbe345dd7f3c9338d356cf1
Andriy <andriy.dudka@gmail.com>
2016-09-29 20:14:19 +0300

    first commit.

---
+++ FirstHomeWork/app/src/main/java/com/example/andriy/firsthomework/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.andriy.firsthomework;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
