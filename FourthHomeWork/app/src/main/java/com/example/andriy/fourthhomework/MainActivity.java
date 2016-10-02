453d8e7120aa1e6dc2b6b69b7ad2b635d19a1bf4
Andriy <andriy.dudka@gmail.com>
2016-10-02 20:34:51 +0300

    fix all.

--- FourthHomeWork/app/src/main/java/com/example/andriy/fourthhomework/MainActivity.java
+++ FourthHomeWork/app/src/main/java/com/example/andriy/fourthhomework/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.andriy.fourthhomework;
 
-import android.content.ContentResolver;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
@@ -9,14 +8,12 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.List;
 
-import Adapters.PersonAdapter;
-import Data.PersonsFactory;
-import Models.Person;
+import com.example.andriy.fourthhomework.adapters.PersonAdapter;
+
+import com.example.andriy.fourthhomework.models.Person;
 
 public class MainActivity extends AppCompatActivity {
-
     private PersonAdapter personAdapter;
     private RecyclerView recyclerView;
     private ArrayList<Person> persons;
@@ -26,28 +23,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         persons = new ArrayList<Person>();
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        personAdapter = new PersonAdapter(this, persons);
 
-        ContentResolver contentResolver = getContentResolver();
         Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
 
-        if (cursor.getCount() > 0){
-            while (cursor.moveToNext()){
-                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                String image = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_URI));
-                String phone = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-
-                persons.add(new Person(name, image, phone));
-            }
+        while (cursor.moveToNext()){
+            String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String image = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_URI));
+            String phone = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            persons.add(new Person(name, image, phone));
         }
 
         cursor.close();
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        //List<Person> persons = PersonsFactory.GetCloneList();
-
-        personAdapter = new PersonAdapter(persons);
-
         recyclerView.setAdapter(personAdapter);
     }
 }
